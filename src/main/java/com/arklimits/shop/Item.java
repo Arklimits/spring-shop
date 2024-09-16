9bb783321ec1badf1ae44b8a22e148a904478e50
Arklimits <Arklimits@users.noreply.github.com>
2024-09-16 21:32:20 +0900

    feat: 테이블 만들기
    Add Item Entity
    Add Post Entity
    
    (close #5)

---
+++ src/main/java/com/arklimits/shop/Item.java
@@ -0,0 +1,16 @@
+package com.arklimits.shop;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @Column(nullable = false)
+    public String title;
+
+    @Column(nullable = false)
+    public Integer price;
+}
