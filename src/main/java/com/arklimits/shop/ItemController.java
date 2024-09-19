5067a97493f83d88210f6d766d69c7a7b0894793
Arklimits <arklimits@github.com>
2024-09-19 21:15:39 +0900

    feat: 상세페이지 만들기 1 (#12)
    @PathVariable로 URL 파라미터 수신
    detail.html 구현

--- src/main/java/com/arklimits/shop/ItemController.java
+++ src/main/java/com/arklimits/shop/ItemController.java
@@ -4,10 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -31,6 +33,19 @@ public class ItemController {
     String addPost(String title, Integer price) {
         Item item = new Item(title, price);
         itemRepository.save(item);
+        return "redirect:/list";
+    }
+
+    @GetMapping("/detail/{id}")
+    String detail(@PathVariable Long id, Model model) {
+        Optional<Item> result = itemRepository.findById(id);
+
+        if (result.isPresent()) {
+            model.addAttribute("item", result.get());
+
+            return "detail.html";
+        }
+
         return "redirect:/list";
     }
 }
