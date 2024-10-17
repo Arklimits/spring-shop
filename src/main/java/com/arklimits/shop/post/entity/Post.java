78e691196f4844e950757e826a1be25d53e2d0f4
Arklimits <arklimits@github.com>
2024-10-17 17:17:55 +0900

    feat: Post Setter 추가 (#41)

--- src/main/java/com/arklimits/shop/post/entity/Post.java
+++ src/main/java/com/arklimits/shop/post/entity/Post.java
@@ -1,16 +1,23 @@
 package com.arklimits.shop.post.entity;
 
+import com.arklimits.shop.comment.entity.Comment;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 @Entity
 @Getter
+@Setter
 @ToString
 public class Post {
 
@@ -23,4 +30,8 @@ public class Post {
 
     @Column(nullable = false)
     private Date date;
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
 }
