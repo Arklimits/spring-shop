28ecfce1141c66027bc7e3acc1cb579ee9b77c24
Arklimits <arklimits@github.com>
2024-10-19 15:00:21 +0900

    fix: JWT Token 생성 메서드 변경 (#37)

--- src/main/java/com/arklimits/shop/JwtUtil.java
+++ src/main/java/com/arklimits/shop/JwtUtil.java
@@ -1,12 +1,16 @@
 package com.arklimits.shop;
 
+import com.arklimits.shop.member.security.CustomUser;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import java.util.Date;
+import java.util.stream.Collectors;
 import javax.crypto.SecretKey;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -19,17 +23,21 @@ public class JwtUtil {
         Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretCode));
 
     // JWT 만들어주는 함수
-    public static String createToken() {
-
-        String jwt = Jwts.builder()
+    public static String createToken(Authentication auth) {
+        CustomUser user = (CustomUser) auth.getPrincipal();
+        String authorities = auth.getAuthorities().stream().map(
+                GrantedAuthority::getAuthority)
+            .collect(Collectors.joining(","));
+        
+        return Jwts.builder()
             // .claim(이름, 값) 으로 JWT에 데이터 추가 가능
-            .claim("username", "어쩌구")
-            .claim("displayName", "저쩌구")
+            .claim("username", user.getUsername())
+            .claim("displayName", user.getDisplayName())
+            .claim("authorities", authorities)
             .issuedAt(new Date(System.currentTimeMillis()))
             .expiration(new Date(System.currentTimeMillis() + 10000)) //유효기간 10초
             .signWith(key)
             .compact();
-        return jwt;
     }
 
     // JWT 까주는 함수
