b4bae8bdef8911c3a54aeb6d1da2298d2e4b02ba
Arklimits <arklimits@github.com>
2024-10-19 17:12:52 +0900

    fix: SecurityFilterChain 수정 (#38)
    csrf 비활성화
    session 비활성화
    ExceptionTranslationFilter 앞에 JWTFilter 추가

--- src/main/java/com/arklimits/shop/SecurityConfig.java
+++ src/main/java/com/arklimits/shop/SecurityConfig.java
@@ -4,10 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.ExceptionTranslationFilter;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
@@ -30,17 +32,16 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.csrf(csrf -> csrf.csrfTokenRepository(csrfTokenRepository())
-//            .ignoringRequestMatchers("/login")
-//        );
-        http.csrf((csrf) -> csrf.disable());
-        http.sessionManagement((session) -> session
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        );
+        http.csrf(AbstractHttpConfigurer::disable)
 
-        http.authorizeHttpRequests((authorize) -> authorize.requestMatchers("/**").permitAll());
-//        http.formLogin((formLogin) -> formLogin.loginPage("/login").defaultSuccessUrl("/"));
-        http.logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/"));
+            .sessionManagement(
+                (session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+            .addFilterBefore(new JwtFilter(), ExceptionTranslationFilter.class)
+
+            .authorizeHttpRequests((authorize) -> authorize.requestMatchers("/**").permitAll())
+            .formLogin((formLogin) -> formLogin.loginPage("/login").defaultSuccessUrl("/"))
+            .logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/"));
         return http.build();
     }
 }
