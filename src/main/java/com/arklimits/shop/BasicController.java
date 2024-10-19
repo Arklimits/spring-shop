55bc55124c445aa97309ad18c7f319bc7fae6ca9
Arklimits <arklimits@github.com>
2024-10-19 15:28:40 +0900

    refactor: JWT 로그인 기능 BasicController로 이동 (#37)

--- src/main/java/com/arklimits/shop/BasicController.java
+++ src/main/java/com/arklimits/shop/BasicController.java
@@ -1,12 +1,22 @@
 package com.arklimits.shop;
 
 import java.time.LocalDateTime;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequiredArgsConstructor
 public class BasicController {
+
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
     @GetMapping("/")
     String home() {
@@ -23,5 +33,19 @@ public class BasicController {
     @ResponseBody
     LocalDateTime date() {
         return LocalDateTime.now();
+    }
+
+    @PostMapping("/login/jwt")
+    @ResponseBody
+    public String loginJWT(@RequestBody Map<String, String> body) {
+        var authToken = new UsernamePasswordAuthenticationToken(body.get("username"),
+            body.get("password"));
+        var auth = authenticationManagerBuilder.getObject().authenticate(authToken);
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+        var jwt = JwtUtil.createToken(SecurityContextHolder.getContext().getAuthentication());
+        System.out.println(jwt);
+
+        return jwt;
     }
 }
--- src/main/java/com/arklimits/shop/member/controller/MemberController.java
+++ src/main/java/com/arklimits/shop/member/controller/MemberController.java
@@ -1,6 +1,5 @@
 package com.arklimits.shop.member.controller;
 
-import com.arklimits.shop.JwtUtil;
 import com.arklimits.shop.member.dto.MemberDTO;
 import com.arklimits.shop.member.entity.Member;
 import com.arklimits.shop.member.security.CustomUser;
@@ -8,18 +7,13 @@ import com.arklimits.shop.member.service.MemberService;
 import com.arklimits.shop.order.dto.OrderDTO;
 import com.arklimits.shop.order.service.OrderService;
 import java.util.List;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -28,7 +22,6 @@ public class MemberController {
 
     private final MemberService memberService;
     private final OrderService orderService;
-    private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
     @GetMapping("/register")
     public String registerPage() {
@@ -63,19 +56,5 @@ public class MemberController {
     @ResponseBody
     public MemberDTO getUser(@PathVariable Long id) {
         return memberService.getUserById(id);
-    }
-
-    @PostMapping("/login/jwt")
-    @ResponseBody
-    public String loginJWT(@RequestBody Map<String, String> body) {
-        var authToken = new UsernamePasswordAuthenticationToken(body.get("username"),
-            body.get("password"));
-        var auth = authenticationManagerBuilder.getObject().authenticate(authToken);
-        SecurityContextHolder.getContext().setAuthentication(auth);
-
-        var jwt = JwtUtil.createToken(SecurityContextHolder.getContext().getAuthentication());
-        System.out.println(jwt);
-
-        return jwt;
     }
 }
