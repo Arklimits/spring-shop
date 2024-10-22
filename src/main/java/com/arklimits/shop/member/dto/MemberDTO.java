40a8b83effd9d6ace370c5a5a7f9dc7ca0a174ea
Arklimits <arklimits@github.com>
2024-10-22 14:41:04 +0900

    test: Mapper Test

--- src/main/java/com/arklimits/shop/member/dto/MemberDTO.java
+++ src/main/java/com/arklimits/shop/member/dto/MemberDTO.java
@@ -3,15 +3,17 @@ package com.arklimits.shop.member.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
 public class MemberDTO {
 
-    public Long id;
-    public String username;
-    public String displayName;
+    private Long id;
+    private String username;
+    private String displayName;
 }
