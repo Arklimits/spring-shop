5c64b3e4f644744495b2bd80fbcfd303b2b28ece
Arklimits <arklimits@github.com>
2024-11-01 15:08:54 +0900

    refactor: JWT 로그인 멤버컨트롤러로 이동

--- src/main/java/com/arklimits/shop/controller/BasicController.java
+++ src/main/java/com/arklimits/shop/controller/BasicController.java
@@ -2,29 +2,18 @@ package com.arklimits.shop.controller;
 
 import com.arklimits.shop.domain.post.entity.Post;
 import com.arklimits.shop.domain.post.service.PostService;
-import com.arklimits.shop.util.JwtUtil;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
 public class BasicController {
 
-    private final AuthenticationManagerBuilder authenticationManagerBuilder;
     private final PostService postService;
 
     @GetMapping("/")
@@ -45,28 +34,5 @@ public class BasicController {
     @ResponseBody
     LocalDateTime date() {
         return LocalDateTime.now();
-    }
-
-    @PostMapping("/login/jwt")
-    public String loginJWT(@RequestBody Map<String, String> body, HttpServletRequest request,
-        HttpServletResponse response) {
-        var authToken = new UsernamePasswordAuthenticationToken(body.get("username"),
-            body.get("password"));
-        var auth = authenticationManagerBuilder.getObject().authenticate(authToken);
-        SecurityContextHolder.getContext().setAuthentication(auth);
-
-        var jwt = JwtUtil.createToken(SecurityContextHolder.getContext().getAuthentication());
-        System.out.println(jwt);
-
-        Cookie cookie = new Cookie("jwt", jwt);
-        cookie.setMaxAge(60 * 30);
-        cookie.setHttpOnly(true);
-        cookie.setPath("/");
-        response.addCookie(cookie);
-
-        // 이전 페이지로 리다이렉트
-        String referer = request.getHeader("Referer");
-        return "redirect:" + (referer != null ? referer
-            : "/item/list"); // referer가 없을 경우 기본 경로로 이동
     }
 }
--- src/main/java/com/arklimits/shop/domain/member/controller/MemberController.java
+++ src/main/java/com/arklimits/shop/domain/member/controller/MemberController.java
@@ -5,18 +5,28 @@ import com.arklimits.shop.domain.member.security.CustomUser;
 import com.arklimits.shop.domain.member.service.MemberService;
 import com.arklimits.shop.domain.order.dto.OrderDTO;
 import com.arklimits.shop.domain.order.service.OrderService;
+import com.arklimits.shop.util.JwtUtil;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
 
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
     private final MemberService memberService;
     private final OrderService orderService;
 
@@ -28,6 +38,29 @@ public class MemberController {
     @GetMapping("/login")
     public String loginPage() {
         return "member/login";
+    }
+
+    @PostMapping("/login/jwt")
+    public String loginJWT(@RequestBody Map<String, String> body, HttpServletRequest request,
+        HttpServletResponse response) {
+        var authToken = new UsernamePasswordAuthenticationToken(body.get("username"),
+            body.get("password"));
+        var auth = authenticationManagerBuilder.getObject().authenticate(authToken);
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+        var jwt = JwtUtil.createToken(SecurityContextHolder.getContext().getAuthentication());
+        System.out.println(jwt);
+
+        Cookie cookie = new Cookie("jwt", jwt);
+        cookie.setMaxAge(60 * 30);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+
+        // 이전 페이지로 리다이렉트
+        String referer = request.getHeader("Referer");
+        return "redirect:" + (referer != null ? referer
+            : "/item/list"); // referer가 없을 경우 기본 경로로 이동
     }
 
     @GetMapping("/mypage")
