68104871ddb43f92a41263a5cf520ded4bb43ce0
Arklimits <arklimits@github.com>
2024-11-01 14:32:08 +0900

    fix: Main Page에 공지사항 추가

--- src/main/java/com/arklimits/shop/controller/BasicController.java
+++ src/main/java/com/arklimits/shop/controller/BasicController.java
@@ -1,16 +1,20 @@
 package com.arklimits.shop.controller;
 
+import com.arklimits.shop.domain.post.entity.Post;
+import com.arklimits.shop.domain.post.service.PostService;
 import com.arklimits.shop.util.JwtUtil;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,9 +25,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class BasicController {
 
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final PostService postService;
 
     @GetMapping("/")
-    String home() {
+    String home(Model model) {
+
+        List<Post> posts = postService.getAllPosts();
+        model.addAttribute("posts", posts);
         return "main/index";
     }
 
